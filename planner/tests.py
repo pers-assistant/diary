0d2fb48d8c13684e8a62e317a84b9b113ce2e086
Alexey Petrov <adm_sib@mail.ru>
2023-01-30 17:31:02 +0700

    add views for record

--- planner/tests.py
+++ planner/tests.py
@@ -1,8 +1,10 @@
+from datetime import datetime
+import json
+
 from django.test import TestCase
 
 from planner.models import Record
 
-# Create your tests here.
 
 class PlannerAPITest(TestCase):
     """test planners API """
@@ -15,3 +17,17 @@ class PlannerAPITest(TestCase):
         response = self.client.get(self.base_url.format(record.id))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response['content-type'], 'application/json')
+
+    def test_get_returns_record_correct(self):
+        """test: returns correct json record"""
+        dt = datetime.strptime("21/01/23 16:30", "%d/%m/%y %H:%M")
+        record = Record.objects.create(title="Test record", content="test content", сreated_at=dt)
+        good_result = {
+            'id': record.id,
+            'title': 'Test record',
+            'content': 'test content',
+            'created_at': dt
+        }
+        response = self.client.get(self.base_url.format(record.id))
+        print(response.content)
+        self.assertEqual(json.loads(response.content.decode('utf8')), good_result)
--- planner/views.py
+++ planner/views.py
@@ -1,7 +1,21 @@
+import json
+
 from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from planner.models import Record
+
 
 def record(request, record_id: int) -> HttpResponse:
-    return HttpResponse(content_type='application/json')
+    record = Record.objects.get(pk=record_id)
+    record_dict = {
+        'id': record.pk,
+        'title': record.title,
+        'content': record.content,
+        'created_at': record.сreated_at
+    }
+    return HttpResponse(
+        json.dumps(record_dict),
+        content_type='application/json'
+    )
