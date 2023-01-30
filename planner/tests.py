fbeed7665c0199b91944dd5e3a81faacdb3a860c
Alexey Petrov <adm_sib@mail.ru>
2023-01-30 16:16:57 +0700

    add test record

--- planner/tests.py
+++ planner/tests.py
@@ -1,5 +1,17 @@
 from django.test import TestCase
 
+from planner.models import Record
+
 # Create your tests here.
 
+class PlannerAPITest(TestCase):
+    """test planners API """
 
+    base_url = '/api/planner/records/{}'
+
+    def test_get_returns_json_200(self):
+        """test: returns json and status 200"""
+        record = Record.objects.create()
+        response = self.client.get(self.base_url.format(record.id))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['content-type'], 'application/json')
